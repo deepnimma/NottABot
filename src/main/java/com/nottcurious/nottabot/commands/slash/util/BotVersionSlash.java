fdff1224d7ff967cba5207d47d59e3b6077550c9
Deepesh Nimma <deepnimma@gmail.com>
2022-02-25 13:46:11 +0530

    Slash Command for Bot Version

--- src/main/java/com/nottcurious/nottabot/commands/slash/util/BotVersionSlash.java
+++ src/main/java/com/nottcurious/nottabot/commands/slash/util/BotVersionSlash.java
@@ -1,20 +1,23 @@
 package com.nottcurious.nottabot.commands.slash.util;
 
-import com.nottcurious.nottabot.helpers.PingHelper;
+import com.nottcurious.nottabot.helpers.BotVersionHelper;
 import me.s3ns3iw00.jcommands.CommandResponder;
 import me.s3ns3iw00.jcommands.event.listener.CommandActionEventListener;
 import me.s3ns3iw00.jcommands.event.type.CommandActionEvent;
 import me.s3ns3iw00.jcommands.type.ServerCommand;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.javacord.api.DiscordApi;
 import org.javacord.api.entity.message.embed.EmbedBuilder;
 
-public class PingCommandSlash extends ServerCommand implements CommandActionEventListener {
+import java.io.IOException;
+
+public class BotVersionSlash extends ServerCommand implements CommandActionEventListener {
     private static final Logger log = LogManager.getLogger();
 
-    public PingCommandSlash() {
-        super("ping", "Pong!");
-        log.debug("Initializing PingCommandSlash");
+    public BotVersionSlash() {
+        super("botversion", "Gets the bot version");
+        log.debug("Initializing BotVersionSlash");
         setOnAction(this);
     }
 
@@ -22,8 +25,16 @@ public class PingCommandSlash extends ServerCommand implements CommandActionEven
     public void onAction(CommandActionEvent event) {
         CommandResponder responder = event.getResponder();
 
-        log.debug("Getting Ping Embed");
-        EmbedBuilder embed = PingHelper.getPingEmbed();
+        DiscordApi api = event.getChannel().get().getApi();
+
+        log.debug("Getting BotVersion Embed");
+        EmbedBuilder embed = null;
+        try {
+            embed = BotVersionHelper.getBotVersionEmbed(api);
+        } catch (IOException e) {
+            log.fatal(e.getStackTrace());
+//            e.printStackTrace();
+        }
 
         log.info("Responding with PingEmbed");
         responder.respondNow()
